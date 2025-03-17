7b5871b3f9de846574ad90dc7bad771d7dd8e439
Edhey <alu0101552392@ull.edu.es>
2025-03-17 02:04:00 +0000

    Using BigInteger for fix desplazation error

--- DAA_P4/src/main/java/DynamicProgramming/TravelingSalesmanProblem/DynamicProgramming.java
+++ DAA_P4/src/main/java/DynamicProgramming/TravelingSalesmanProblem/DynamicProgramming.java
@@ -1,6 +1,7 @@
 package DynamicProgramming.TravelingSalesmanProblem;
 
 import java.util.ArrayList;
+import java.math.BigInteger;
 
 import DynamicProgramming.Graph.Graph;
 import DynamicProgramming.Graph.Node.Node;
@@ -8,19 +9,19 @@ import DynamicProgramming.Graph.Node.Node;
 public class DynamicProgramming extends TravelingSalesmanProblem {
   final int INF = Integer.MAX_VALUE / 2; // Para evitar overflow
   private int[][] dist = null;
-  private int[][] dp = null;
   private int[][] parent = null;
+  private BigInteger[][] dp = null; // Cambiamos dp a BigInteger para manejar máscaras grandes
   private int numberOfNodes = 0;
 
   @Override
   public ArrayList<Node> solve(Graph graph, String startNode) {
     this.dist = graphToMatrix(graph);
     this.numberOfNodes = dist.length;
-    this.dp = new int[1 << numberOfNodes][numberOfNodes];
+    this.dp = new BigInteger[1 << numberOfNodes][numberOfNodes];
     this.parent = new int[1 << numberOfNodes][numberOfNodes];
-    for (int[] row : dp) {
+    for (BigInteger[] row : dp) {
       for (int i = 0; i < row.length; i++) {
-        row[i] = -1;
+        row[i] = BigInteger.valueOf(-1);
       }
     }
     for (int[] row : parent) {
@@ -28,7 +29,7 @@ public class DynamicProgramming extends TravelingSalesmanProblem {
         row[i] = -1;
       }
     }
-    this.pathCost = recursiveSolve(1, 0);
+    this.pathCost = recursiveSolve(BigInteger.ONE, 0);
     ArrayList<Integer> optimalPath = getOptimalPath();
     ArrayList<Node> path = new ArrayList<>();
     ArrayList<Node> nodes = new ArrayList<>(graph.getNodes());
@@ -38,43 +39,47 @@ public class DynamicProgramming extends TravelingSalesmanProblem {
     return path;
   }
 
-  private int recursiveSolve(int mask, int pos) {
-    if (mask == (1 << numberOfNodes) - 1)
-      return dist[pos][0]; // Volver al inicio
+  private int recursiveSolve(BigInteger mask, int pos) {
+    if (mask.equals(BigInteger.ONE.shiftLeft(numberOfNodes).subtract(BigInteger.ONE))) {
+      return dist[pos][0]; // Return to starting city
+    }
 
-    if (dp[mask][pos] != -1)
-      return dp[mask][pos];
+    if (!dp[mask.intValue()][pos].equals(BigInteger.valueOf(-1))) {
+      return dp[mask.intValue()][pos].intValue();
+    }
 
     int minCost = INF;
     int bestPrev = -1; // Para rastrear el camino óptimo
 
     for (int next = 0; next < numberOfNodes; next++) {
-      if ((mask & (1 << next)) == 0) { // Si la ciudad no ha sido visitada
-        int newCost = dist[pos][next] + recursiveSolve(mask | (1 << next), next);
+      if (mask.and(BigInteger.ONE.shiftLeft(next)).equals(BigInteger.ZERO)) { // If city is not visited
+        int newCost = dist[pos][next] + recursiveSolve(mask.or(BigInteger.ONE.shiftLeft(next)), next);
         if (newCost < minCost) {
           minCost = newCost;
-          bestPrev = next; // Guardamos la mejor ciudad previa
+          bestPrev = next; // Save the best decision so far
         }
       }
     }
-    parent[mask][pos] = bestPrev; // Guardamos la mejor decisión
-    return dp[mask][pos] = minCost;
+    parent[mask.intValue()][pos] = bestPrev; // Save the best decision
+    dp[mask.intValue()][pos] = BigInteger.valueOf(minCost);
+    return minCost;
   }
 
-    public ArrayList<Integer> getOptimalPath() {
-        ArrayList<Integer> path = new ArrayList<>();
-        int mask = 1, pos = 0; // Comenzamos desde la ciudad 0
+  public ArrayList<Integer> getOptimalPath() {
+    ArrayList<Integer> path = new ArrayList<>();
+    BigInteger mask = BigInteger.ONE;
+    int pos = 0; // Start from city 0
 
-        while (true) {
-            path.add(pos);
-            int next = parent[mask][pos];
-            if (next == -1) break; // Ya terminamos el recorrido
-            mask |= (1 << next);
-            pos = next;
-        }
-        path.add(0); // Volver a la ciudad inicial
-        return path;
+    while (true) {
+      path.add(pos);
+      int next = parent[mask.intValue()][pos];
+      if (next == -1) break; // There is no next city
+      mask = mask.or(BigInteger.ONE.shiftLeft(next));
+      pos = next;
     }
+    path.add(0); // Return to starting city
+    return path;
+  }
 
   static public int[][] graphToMatrix(Graph graph) {
     int[][] matrix = new int[graph.getNodes().size()][graph.getNodes().size()];
